44fe19dea43d2f9c185c3c02568f46259a455082
Jessy Vanderaugstraete <jessy.vanderaugstraete@ig2i.centralelille.fr>
2019-09-20 11:31:46 +0200

    [FIX] Wrong module name prevents loading routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { PokemonsModule } from './pokemons/pokemons.module';
+
 const routes: Routes = [
 	{
 		path: 'pokemons',
-		loadChildren: () => import('./pokemons/pokemons.module').then((m: any) => m.PokemonModule),
+		loadChildren: () => import('./pokemons/pokemons.module').then((m: any) => m.PokemonsModule),
 	},
 ];
 
